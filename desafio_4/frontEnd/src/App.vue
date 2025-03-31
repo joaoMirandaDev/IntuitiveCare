42e70f0a3f7751f617a14626b30cba5cf347cbb2
Miranda <joao_to10@hotmail.com>
2025-03-31 20:40:00 -0300

    #Melhorias   no README

--- desafio_4/frontEnd/src/App.vue
+++ desafio_4/frontEnd/src/App.vue
@@ -2,34 +2,25 @@
 div
   div(style="margin-bottom: 0.5rem; display: flex; justify-content: center;")
     el-text(class="mx-1" style="font-size: 38px;") Busca de Operadoras
-  el-input(
-    v-model="query"
-    style="width: 240px; margin-bottom: 0.5rem;"
-    placeholder="Digite um termo para buscar"
-    clearable
-    @input="searchOperadoras"
-  )
-  
-  div
-    el-table(:data="operadoras" border style="width: 100%  ")
-      el-table-column(prop="Registro_ANS" label="Registro ANS" width='150px')
-      el-table-column(prop="CNPJ" label="CNPJ" width="140")
-      el-table-column(prop="Razao_Social" label="Razão Social" width='440px')
-      el-table-column(prop="Modalidade" label="Modalidade")
-      el-table-column(prop="Telefone" label="Telefone")
-        template(#default="{ row }")
-          span {{ row.Telefone != null ? '(' +row.DDD + ') ' + row.Telefone : '-' }}
-      el-table-column(label="Cidade" )
-        template(#default="{ row }")
-          span {{ row.UF + '/' + row.Cidade }}
-      el-table-column(prop="Representante" label="Representante")
-    el-pagination(style="margin-top: 20px; display: flex; justify-content: flex-end;"
-      background layout="prev, pager, next" :total="1000" 
-      @change="handlePage"
-      :page-count="totalPage"
-      :default-current-page="1"
-      :page="page"
+  el-card(style="padding: 0; margin-top: 1rem;")
+    div(style="justify-content: end; position: relative; display: flex;")
+      el-input(
+        v-model="query"
+        style="width: 300px; margin-bottom: 0.5rem;"
+        placeholder="Digite um termo para buscar"
+        clearable
+        @input="searchOperadoras"
       )
+    div
+      el-table(:data="operadoras" border style="width: 100%  ")
+        el-table-column(v-for="col in columns" :prop="col.prop" :label="col.label" :width='col.width')
+      el-pagination(style="margin-top: 20px; display: flex; justify-content: flex-end;"
+        background layout="prev, pager, next" 
+        @change="handlePage"
+        :page-count="totalPage"
+        :default-current-page="1"
+        :page="page"
+        )
 </template>
 
 <script>
@@ -39,6 +30,15 @@ export default {
   name: 'App',
   data() {
     return {
+      columns: [
+        { prop: "Registro_ANS", label: "Registro ANS", width: "150px"},
+        { prop: "CNPJ", label: "CNPJ Social", width: "140"},
+        { prop: "Razao_Social", label: "Razão Social", width: "440px"},
+        { prop: "Modalidade", label: "Modalidade" },
+        { prop: "Telefone", label: "Telefone"},
+        { prop: "Cidade", label: "Cidade"},
+        { prop: "Representante", label: "Representante"},
+      ],
       page: 1,
       totalResult: 0,
       pageSize: 10, 
@@ -75,16 +75,5 @@ export default {
 #app {
   font-family: Arial, sans-serif;
   padding: 20px;
-}
-
-input {
-  padding: 10px;
-  font-size: 16px;
-  margin-bottom: 20px;
-  width: 300px;
-}
-
-h2 {
-  margin-top: 20px;
 }
 </style>
